3e462560d00ff586a74f220ac4181b029b9c17eb
CHANDRADITHYA K.G <64347511+Chandradithya8@users.noreply.github.com>
2021-11-08 16:49:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('Home.html')
 
 
 @app.route('/crop_recommend')
